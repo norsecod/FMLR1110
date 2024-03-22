8140f48a6b648cae7a3cbfa2138f3415ef72f64b
norsecod <tgaarden91@gmail.com>
2024-03-22 18:16:39 +0100

    mar22 18:16

--- application/application.mk
+++ application/application.mk
@@ -3,17 +3,10 @@ USER_APP_C_DEFS = \
 USER_APP_C_SOURCES = \
 	application/main.c \
 	extensions/drivers/cayennelpp/cayenne_lpp.c \
-	extensions/drivers/led/led.c \
-	extensions/drivers/button/button.c \
-	extensions/drivers/sht3x/sht3x.c \
-	extensions/drivers/lis2dw/lis2dw.c \
 
 USER_APP_C_INCLUDES =  \
 	-Iapplication \
 	-extensions/drivers/cayennelpp\
-	-Iextensions/drivers/led\
-	-Iextensions/drivers/button\
-	-Iextensions/drivers/sht3x\
-	-Iextensions/drivers/lis2dw\
+
 
 USER_APP_ASM_SOURCES = \
