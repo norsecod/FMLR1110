29fbe20d730dfc6792310b518a10b5e751885921
norsecod <tgaarden91@gmail.com>
2024-04-09 13:44:30 +0200

    update comments function.c

--- extensions/drivers/bachelor/functions.c
+++ extensions/drivers/bachelor/functions.c
@@ -24,12 +24,12 @@ extern TimerEvent_t water_timer, door_timer;
     };
 
 
-#define STACK_ID 0
+#define STACK_ID 0  
 
 
-void sensor_read(void) {
-    SMTC_HAL_TRACE_PRINTF("----- sensor_read -----\n\r");
-    temp = GETtemperature(1);
+void sensor_read(void) {                                    //function that calls functions for sensors
+    SMTC_HAL_TRACE_PRINTF("----- sensor_read -----\n\r");   //and calculates the real value of battery
+    temp = GETtemperature(1);                               //adc measurment
     ADCmeas = GETvoltage(&hadc);
     Voltage = ADCmeas * VDR;   
 
@@ -38,39 +38,42 @@ void sensor_read(void) {
 #endif
 }
 
-void printCayenneLPPBuffer(const cayenne_lpp_t *lpp) {
-    SMTC_HAL_TRACE_PRINTF("CayenneLPP Buffer Content (Hex): ");
-    for (uint8_t i = 0; i < lpp->cursor; ++i) {
-        SMTC_HAL_TRACE_PRINTF("%02X ", lpp->buffer[i]);
+void printCayenneLPPBuffer(const cayenne_lpp_t *lpp) 
+{
+    SMTC_HAL_TRACE_PRINTF("CayenneLPP Buffer Content (Hex): "); //function that prints the content of
+    for (uint8_t i = 0; i < lpp->cursor; ++i) {                 // cayennelpp buffer so we know it is filled
+        SMTC_HAL_TRACE_PRINTF("%02X ", lpp->buffer[i]);         //used for debuggig
     }
     SMTC_HAL_TRACE_PRINTF("\n\r");
 }
 
-void sendLoRaWANPacket(const uint8_t *payload, uint8_t payloadSize) {
-    uint8_t port = 85;
-    bool confirmed = false;
-    smtc_modem_request_uplink(STACK_ID, port, confirmed, payload, payloadSize);
+void sendLoRaWANPacket(const uint8_t *payload, uint8_t payloadSize) //frame function for uplink.
+{   
+    uint8_t port = 85; //predetermined fport for LoRaWAN uplink
+    bool confirmed = false; //unconfirmed package
+    smtc_modem_request_uplink(STACK_ID, port, confirmed, payload, payloadSize); // sends data over LoRaWAN
 }
 
 
 
-void sendData(float temperature, float analogValue, bool digitalValue1, bool digitalValue2) {
-    cayenne_lpp_t lpp;
-    cayenne_lpp_reset(&lpp);
+void sendData(float temperature, float analogValue, bool digitalValue1, bool digitalValue2)
+ {
+    cayenne_lpp_t lpp;     //creates and name a structure for CayenneLPP named "lpp" contains buffer and cursor
+    cayenne_lpp_reset(&lpp);    //resets the structure lpp so it is empty
 
-    cayenne_lpp_add_temperature(&lpp, 1, temperature);
-    cayenne_lpp_add_analog_input(&lpp, 2, analogValue);
-    cayenne_lpp_add_digital_input(&lpp, 3, digitalValue1);
-    cayenne_lpp_add_digital_input(&lpp, 4, digitalValue2);
+    cayenne_lpp_add_temperature(&lpp, 1, temperature);  //temperature channel 1
+    cayenne_lpp_add_analog_input(&lpp, 2, analogValue); //ADC (analog) value on channel 2
+    cayenne_lpp_add_digital_input(&lpp, 3, digitalValue1);  //Door magnet on channel 3 (high or low)
+    cayenne_lpp_add_digital_input(&lpp, 4, digitalValue2);  //watersensor on channel 4 (high or low)
 
 
-    sendLoRaWANPacket(lpp.buffer, lpp.cursor);
+    sendLoRaWANPacket(lpp.buffer, lpp.cursor); //payload in buffer, size is in cursor. is a framefunction for uplink
     printCayenneLPPBuffer(&lpp); // Used only for debugging
 }
 
-void wateralarm(bool digitalValue)
-{ 
-    cayenne_lpp_t lpp;
+void wateralarm(bool digitalValue)                                  //used for the interrupt routine
+{                                                                   //to send state of water sensor
+    cayenne_lpp_t lpp;                                              //look at "sendData for more info"
     cayenne_lpp_reset(&lpp);
     cayenne_lpp_add_digital_input(&lpp, 4, digitalValue);
 
@@ -78,9 +81,9 @@ void wateralarm(bool digitalValue)
     printCayenneLPPBuffer(&lpp); // Used only for debugging
 }
 
-void dooralarm(bool digitalValue)
-{
-    cayenne_lpp_t lpp;
+void dooralarm(bool digitalValue)                                   //used for the interrupt routine        
+{                                                                   //to send state of door sensor
+    cayenne_lpp_t lpp;                                              //look at "sendData for more info"
     cayenne_lpp_reset(&lpp);
     cayenne_lpp_add_digital_input(&lpp, 3, digitalValue);
 
@@ -88,17 +91,17 @@ void dooralarm(bool digitalValue)
     printCayenneLPPBuffer(&lpp); // Used only for debugging
 }
 
-float GETtemperature(const uint32_t id) {
-    uint8_t device_address = 0x90;
-    uint8_t temperature_raw;
+float GETtemperature(const uint32_t id) {    //function that calls TC74A0, and reads the temperature
+    uint8_t device_address = 0x90;           //adress of TC74A0                    
+    uint8_t temperature_raw;                 
     float temperature;
 
-    if (hal_i2c_read(id, device_address, 0x00, &temperature_raw) != SUCCESS) {
-        return -128.0f; // Error value
+    if (hal_i2c_read(id, device_address, 0x00, &temperature_raw) != SUCCESS) {  //reads the temperature 
+        return -128.0f; // Error value                                          if error occurs returns -128 as temperature
     }
 
-    temperature = (float)((int8_t)temperature_raw);
-    return temperature;
+    temperature = (float)((int8_t)temperature_raw); //typecasts the int8_t data to float data and stores it in temperature
+    return temperature; 
 }
 
 float GETvoltage(ADC_HandleTypeDef *hadc) {
